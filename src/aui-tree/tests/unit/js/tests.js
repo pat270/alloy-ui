c868b1384d88521050227d8b02bab17e1a350658
Patrick Yeo <patrick.yeo@liferay.com>
2013-11-26 17:06:38 -0800

    AUI-1044 - Tests for TreeNodeRadio and TreeNodeTask

--- src/aui-tree/tests/unit/js/tests.js
+++ src/aui-tree/tests/unit/js/tests.js
@@ -30,7 +30,171 @@ YUI.add('module-tests', function(Y) {
                     id: 'three'
                 }
             ]
+        }).render();
+    };
+
+    var createNewTreeViewRadio = function(string) {
+        var selectAll = (string === 'selectAllChildren') ? true : false;
+
+        var apricotNode = new Y.TreeNodeRadio({
+            expanded: true,
+            id: 'apricotNode',
+            label: 'Apricot',
+            leaf: false,
+            selectAllChildren: selectAll
         });
+
+        var fruitNode = new Y.TreeNodeRadio({
+            expanded: true,
+            id: 'fruitNode',
+            label: 'Fruit',
+            leaf: false,
+            selectAllChildren: selectAll
+        });
+
+        var radioTree = new Y.TreeView({
+            id: 'testRadioButton'
+        });
+
+        var radioTreeChild = new Y.TreeNodeRadio({
+            expanded: true,
+            id: 'testRadioNode',
+            label: 'Radio',
+            selectAllChildren: selectAll
+        });
+
+        var seedsNode = new Y.TreeNodeRadio({
+            id: 'seedsNode',
+            label: 'Seeds',
+            leaf: true,
+            selectAllChildren: selectAll
+        });
+
+        var watermelonNode = new Y.TreeNodeRadio({
+            expanded: true,
+            id: 'watermelonNode',
+            label: 'Watermelon',
+            leaf: false,
+            selectAllChildren: selectAll
+        });
+
+        radioTree.appendChild(radioTreeChild);
+
+        radioTreeChild.appendChild(fruitNode);
+
+        fruitNode.appendChild(apricotNode);
+
+        fruitNode.appendChild(watermelonNode);
+
+        apricotNode.appendChild(seedsNode);
+
+        radioTree.render();
+
+        return radioTree;
+    };
+
+    var createNewTreeViewTask = function(string) {
+        var selectAll = (string === 'selectAllChildren') ? true : false;
+
+        var apricotNode = new Y.TreeNodeTask({
+            expanded: true,
+            id: 'apricotNode',
+            label: 'Apricot',
+            leaf: false,
+            selectAllChildren: selectAll
+        });
+
+        var fruitNode = new Y.TreeNodeTask({
+            expanded: true,
+            id: 'fruitNode',
+            label: 'Fruit',
+            leaf: false,
+            selectAllChildren: selectAll
+        });
+
+        var taskTree = new Y.TreeView({
+            id: 'testTaskButton'
+        });
+
+        var taskTreeChild = new Y.TreeNodeTask({
+            expanded: true,
+            id: 'testTaskNode',
+            label: 'Task',
+            selectAllChildren: selectAll
+        });
+
+        var seedsNode = new Y.TreeNodeTask({
+            id: 'seedsNode',
+            label: 'Seeds',
+            leaf: true,
+            selectAllChildren: selectAll
+        });
+
+        var watermelonNode = new Y.TreeNodeTask({
+            expanded: true,
+            id: 'watermelonNode',
+            label: 'Watermelon',
+            leaf: false,
+            selectAllChildren: selectAll
+        });
+
+        taskTree.appendChild(taskTreeChild);
+
+        taskTreeChild.appendChild(fruitNode);
+
+        fruitNode.appendChild(apricotNode);
+
+        fruitNode.appendChild(watermelonNode);
+
+        apricotNode.appendChild(seedsNode);
+
+        taskTree.render();
+
+        return taskTree;
+    };
+
+    var childrenShouldBeChecked = function(node) {
+        node.all('.tree-node').each(
+            function(node) {
+                Y.Assert.isTrue(
+                    node.one('.tree-node-content').hasClass('tree-node-checked'),
+                    '#' + node.get('id') + ' should be checked');
+            }
+        );
+    };
+
+    var childrenShouldNotBeChecked = function(node) {
+        node.all('.tree-node').each(
+            function(node) {
+                Y.Assert.isFalse(
+                    node.one('.tree-node-content').hasClass('tree-node-checked'),
+                    '#' + node.get('id') + ' should not be checked');
+            }
+        );
+    };
+
+    var parentsShouldBeChecked = function(node) {
+        var parentNode = node.ancestor('.tree-node');
+
+        if (parentNode) {
+            Y.Assert.isTrue(
+                parentNode.one('.tree-node-content').hasClass('tree-node-checked'),
+                '#' + parentNode.get('id') + ' should be checked.');
+
+            parentsShouldBeChecked(parentNode);
+        }
+    };
+
+    var parentsShouldNotBeChecked = function(node) {
+        var parentNode = node.ancestor('.tree-node');
+
+        if (parentNode) {
+            Y.Assert.isFalse(
+                parentNode.one('.tree-node-content').hasClass('tree-node-checked'),
+                '#' + parentNode.get('id') + ' should not be checked.');
+
+            parentsShouldNotBeChecked(parentNode);
+        }
     };
 
     suite.add(new Y.Test.Case({
@@ -170,11 +334,73 @@ YUI.add('module-tests', function(Y) {
             var node = new Y.TreeNode();
 
             Y.Assert.isFalse(tree.isRegistered(node), 'TreeNode should be registered in TreeView');
+        },
+
+        'TreeNodeRadio should not select all children by default': function() {
+            var testRadio = createNewTreeViewRadio();
+
+            Y.one('#testRadioButton').all('.tree-node').each(
+                function(node) {
+                    var checkedNodeId = node.get('id');
+
+                    node.one('.tree-node-checkbox-container').simulate('click');
+
+                    parentsShouldNotBeChecked(node);
+
+                    childrenShouldNotBeChecked(node);
+                }
+            );
+        },
+
+        'TreeNodeRadio should select all children if selectAllChildren is true': function() {
+            var testTree = createNewTreeViewRadio('selectAllChildren');
+
+            Y.one('#testRadioButton').all('.tree-node').each(
+                function(node){
+                    node.one('.tree-node-checkbox-container').simulate('click');
+
+                    parentsShouldNotBeChecked(node);
+
+                    childrenShouldBeChecked(node);
+
+                    node.one('.tree-node-checkbox-container').simulate('click');
+                }
+            );
+        },
+
+        'TreeNodeTask should not select all children by default': function() {
+            var testTask = createNewTreeViewTask();
+
+            Y.one('#testTaskButton').all('.tree-node').each(
+                function(node) {
+                    node.one('.tree-node-checkbox-container').simulate('click');
+
+                    parentsShouldBeChecked(node);
+
+                    childrenShouldNotBeChecked(node);
+                }
+            );
+        },
+
+        'TreeNodeTask should select all children if selectAllChildren is true': function() {
+            var testTask = createNewTreeViewTask('selectAllChildren');
+
+            Y.one('#testTaskButton').all('.tree-node').each(
+                function(node) {
+                    node.one('.tree-node-checkbox-container').simulate('click');
+
+                    childrenShouldBeChecked(node);
+
+                    parentsShouldNotBeChecked(node);
+
+                    node.one('.tree-node-checkbox-container').simulate('click');
+                }
+            );
         }
     }));
 
     Y.Test.Runner.add(suite);
 
 }, '', {
-    requires: ['aui-tree', 'test']
+    requires: ['aui-tree', 'node-event-simulate', 'test']
 });
